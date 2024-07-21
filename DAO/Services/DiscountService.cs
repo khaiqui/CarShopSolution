48aeccefd3ab3b73f81450d473e05996024912a8
TDHanie <luongngocthuyduongvt@gmail.com>
2024-07-21 17:59:32 +0700

    Chỉnh sửa DetailWindow, và thêm các chức năng Create và Update

---
+++ DAO/Services/DiscountService.cs
@@ -0,0 +1,28 @@
+﻿using DTO.Models;
+using DTO.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Services
+{
+	public class DiscountService
+	{
+		private DiscountRepository _repo = new();
+		public List<Discount> GetAllDiscount()
+		{
+			return _repo.GetAll();
+		}
+		public void Create02(Product x)
+		{
+			_repo.Create(x);
+		}
+
+		public void Update02(Product x)
+		{
+			_repo.Update(x);
+		}
+	}
+}
